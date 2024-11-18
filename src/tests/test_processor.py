2e54069ba8b11294854556771bc1c071e8943289
Juan Pablo Rojas Bustos <jrojas9206@gmail.com>
2024-11-18 17:08:03 +0300

    update test files

--- src/tests/test_processor.py
+++ src/tests/test_processor.py
@@ -19,11 +19,10 @@ path2rgbImage = Path(os.path.join(root,
                      '../../data/CPR-ATL-KET_control/Images/RGB_image.png')).resolve()
 path2measurements = Path(os.path.join(root,
                          '../../data/')).resolve()
+
 def test_reflectancePointLoader():
     oRP = ReflectancePointLoader(path2test)
     df = oRP.getResults().head()
-    
-    print(oRP.getSetName(separator='\\'))
     plotReflectanceSignature(df,
                              idx2skip=0,
                              title='Soil Reflectance Signature: CPR-ATL-KET_control',
@@ -40,20 +39,16 @@ lstValues, lstNames = LoadMeasurements(path2measurements)
 
 sc_ex = SpectralCube(path2spectralCube)
 
-print(sc_ex.getCube().shape)
-
 df = sc_ex.getSignature([[500, 500], [1000, 1000]], cSize=2)
-
-print(df.head())
 
 plotReflectanceSignature(df,
                          title='Soil Reflectance Signature: CPR-ATL-KET_control',
                          ylabel='Pixel Values',
-                         avgPlot=True)
+                         avgPlot=False)
 
 
 img = cv2.imread(path2rgbImage, cv2.COLOR_BGR2RGB)
-print(img.dtype)
+
 segmented = manualCropPetriDishe(img,
                      [500, 500],
                      100,
@@ -65,16 +60,14 @@ mask = manualCropPetriDishe(img,
                      100,
                      True,
                      )
-mImg = sc_ex.getCube()
 
-print(mImg.shape)
+mImg = sc_ex.getCube()
 
 histSoilSample(mImg, 
                mask, 
                legend=sc_ex.getBands())
+plt.figure()
+plt.imshow(img)
+plt.figure()
+plt.imshow(segmented)
 plt.show()
-# plt.figure()
-# plt.imshow(img)
-# plt.figure()
-# plt.imshow(segmented)
-# plt.show()
