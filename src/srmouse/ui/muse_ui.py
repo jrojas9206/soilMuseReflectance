093da4b1eacffe4962f7c7e1367009dcaa1867c2
Juan Pablo Rojas Bustos <jrojas9206@gmail.com>
2024-11-18 10:25:25 +0300

    initial def module

--- src/srmouse/ui/muse_ui.py
+++ src/srmouse/ui/muse_ui.py
@@ -5,7 +5,15 @@ from PyQt5 import QtCore as qtc
 
 class MuseApp(qtw.QMainWindow):
 
+    MAIN_WINDOW_HEIGHT = 480
+    MAIN_WINDOW_WIDTH = 720
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Muse Tool")
+        self.setFixedSize(self.MAIN_WINDOW_WIDTH,
+                          self.MAIN_WINDOW_HEIGHT)
+        central_widget = qtw.QWidget()
+        self.setCentralWidget(central_widget)
+        mw_layout = qtw.QGridLayout(central_widget)
         self.show()
