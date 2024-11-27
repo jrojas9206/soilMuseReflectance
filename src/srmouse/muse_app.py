25f3156105dff8b2d4012b920445b49e9e8c726a
Juan Pablo Rojas Bustos <jrojas9206@gmail.com>
2024-11-27 17:53:09 +0300

    deleted old code

--- src/srmouse/muse_app.py
+++
@@ -1,8 +0,0 @@
-import sys 
-from PyQt5 import QtWidgets as qtw
-from srmouse.ui.muse_ui import MuseApp
-
-if __name__ == '__main__':
-    app = qtw.QApplication(sys.argv)
-    mw = MuseApp()
-    sys.exit(app.exec())
